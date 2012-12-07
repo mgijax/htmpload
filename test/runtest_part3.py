756ec902c707d566c49381a0a862bad8a9d074a3
Lori Corbani <Lori.Corbani@jax.org>
2012-12-07 19:03:42 +0000

    TR10273

--- test/runtest_part3.py
+++ test/runtest_part3.py
@@ -745,7 +745,7 @@ def verifySexNA():
 	%s
 	''' % (mclQuery1, markerID, alleleID, mpID, createdby, mclQuery2)
 
-    print query
+    #print query
     results = db.sql(query, 'auto')
     if len(results) > 0:
         testPassed = 'pass'
@@ -810,7 +810,7 @@ def verifySexFemale():
 	%s
 	''' % (mclQuery1, markerID, alleleID, mpID, createdby, mclQuery2)
 
-    print query
+    #print query
     results = db.sql(query, 'auto')
     if len(results) > 0:
         testPassed = 'pass'
@@ -875,7 +875,7 @@ def verifySexMale():
 	%s
 	''' % (mclQuery1, markerID, alleleID, mpID, createdby, mclQuery2)
 
-    print query
+    #print query
     results = db.sql(query, 'auto')
     if len(results) > 0:
         testPassed = 'pass'
